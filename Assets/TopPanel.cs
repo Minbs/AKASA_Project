ba9d514fda96e90deb300220468dd611aabbad85
Minbs <48088677+Minbs@users.noreply.github.com>
2022-04-04 21:29:25 +0900

    크리틱 영상 제출용

--- Assets/TopPanel.cs
+++ Assets/TopPanel.cs
@@ -35,46 +35,71 @@ public class TopPanel : MonoBehaviour
             EnemeyCount();
             //UnitCount();
         }
-    }
-
-    void Timer()
-    {
-        time -= Time.deltaTime;
-
-        min = (int)time / 60;
-        sec = ((int)time - min * 60) % 60;
-
-        if (min <= 0 && sec <= 0)
+        else if (GameManager.Instance.state == State.WAIT)
         {
-            timeText[0].text = 0.ToString();
-            timeText[1].text = 0.ToString();
-        }
-        else
-        {
-            if (sec >= 60)
+            float time = GameManager.Instance.waitTimer;
+            min = (int)time / 60;
+            sec = ((int)time - min * 60) % 60;
+
+            if (min <= 0 && sec <= 0)
             {
-                min += 1;
-                sec -= 60;
+                timeText[0].text = 0.ToString();
+                timeText[1].text = 0.ToString();
             }
             else
             {
-                timeText[0].text = min.ToString();
-                timeText[1].text = sec.ToString();
+                if (sec >= 60)
+                {
+                    min += 1;
+                    sec -= 60;
+                }
+                else
+                {
+                    timeText[0].text = min.ToString();
+                    timeText[1].text = sec.ToString();
+                }
             }
         }
-    }
 
-    void EnemeyCount()
-    {
-        currentEnemyCount = enemiesList.Count;
-        if (currentEnemyCount >= 0)
+        void Timer()
         {
-            timeText[2].text = currentEnemyCount.ToString();
+            time -= Time.deltaTime;
+
+            min = (int)time / 60;
+            sec = ((int)time - min * 60) % 60;
+
+            if (min <= 0 && sec <= 0)
+            {
+                timeText[0].text = 0.ToString();
+                timeText[1].text = 0.ToString();
+            }
+            else
+            {
+                if (sec >= 60)
+                {
+                    min += 1;
+                    sec -= 60;
+                }
+                else
+                {
+                    timeText[0].text = min.ToString();
+                    timeText[1].text = sec.ToString();
+                }
+            }
         }
-    }
 
-    void UnitCount()
-    { 
+        void EnemeyCount()
+        {
+            currentEnemyCount = enemiesList.Count;
+            if (currentEnemyCount >= 0)
+            {
+                timeText[2].text = currentEnemyCount.ToString();
+            }
+        }
 
+        void UnitCount()
+        {
+
+        }
     }
 }
