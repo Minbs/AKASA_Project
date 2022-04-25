791c1e1372023ab24471b6b2db874545504eec18
ryuGH830 <89832327+ryuGH830@users.noreply.github.com>
2022-04-26 00:51:06 +0900

    비전투UI - 로딩 할 때마다 랜덤으로 일러스트 이미지 변경

--- Assets/Scripts/UI/SceneLoad.cs
+++ Assets/Scripts/UI/SceneLoad.cs
@@ -7,14 +7,53 @@ using UnityEngine.SceneManagement;
 public class SceneLoad : MonoBehaviour
 {
     public Slider progressbar;
-    public Text loadtext;
-
     static string nextSceneName;
+
+    int random;
+
+    public GameObject illust;
+    [SerializeField]
+    List<GameObject> illustObj;
+    [SerializeField]
+    List<Image> illustImage;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("로딩 시작");
+        random = Random.Range(0, 4);
+
+        for (int i = 0; i < illust.transform.childCount; i++)
+        {
+            illustObj.Add(illust.transform.GetChild(i).gameObject);
+            illustImage.Add(illustObj[i].GetComponentInChildren<Image>());
+        }
+
+        switch (random)
+        {
+            case 0:
+                illustImage[1].gameObject.SetActive(false);
+                illustImage[2].gameObject.SetActive(false);
+                illustImage[3].gameObject.SetActive(false);
+                break;
+            case 1:
+                illustImage[0].gameObject.SetActive(false);
+                illustImage[2].gameObject.SetActive(false);
+                illustImage[3].gameObject.SetActive(false);
+                break;
+            case 2:
+                illustImage[0].gameObject.SetActive(false);
+                illustImage[1].gameObject.SetActive(false);
+                illustImage[3].gameObject.SetActive(false);
+                break;
+            case 3:
+                illustImage[0].gameObject.SetActive(false);
+                illustImage[1].gameObject.SetActive(false);
+                illustImage[2].gameObject.SetActive(false);
+                break;
+            default:
+                break;
+        }
+
         StartCoroutine(LoadSceneProcess());
     }
 
@@ -22,7 +61,6 @@ public class SceneLoad : MonoBehaviour
     {
         nextSceneName = sceneName;
         SceneManager.LoadScene("LoadingScene");
-        Debug.Log("로딩 끝");
     }
 
     IEnumerator LoadSceneProcess()
@@ -37,19 +75,12 @@ public class SceneLoad : MonoBehaviour
             yield return null;
 
             if (progressbar.value < 0.9f)
-            {
                 progressbar.value = Mathf.MoveTowards(progressbar.value, 0.9f, Time.deltaTime);
-            }
             else if (operation.progress >= 0.9f)
-            {
                 progressbar.value = Mathf.MoveTowards(progressbar.value, 1f, Time.deltaTime);
-            }
 
             if (progressbar.value >= 1f)
-            {
-                loadtext.text = "Next Scene!";
                 operation.allowSceneActivation = true;
-            }
         }
     }
 }
