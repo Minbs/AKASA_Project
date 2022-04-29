e94892ada7b254ce6c1b80656754faa6baa3e7ee
ryuGH830 <89832327+ryuGH830@users.noreply.github.com>
2022-04-29 15:32:04 +0900

    전투UI (진행중) - 미니언 강화UI 크기조절 및 회전조절 구현

--- Assets/Scripts/UI/Deployment.cs
+++ Assets/Scripts/UI/Deployment.cs
@@ -8,30 +8,27 @@ public class Deployment : MonoBehaviour
     public GameObject deploy;
     Image deployRange;
     bool isCheck;
-    Vector3 min, max;
+    float time = 0, rotate = 360;
+
     [SerializeField]
-    float speed = 5.0f;
+    int rotateSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         deployRange = deploy.GetComponentInChildren<Image>();
 
-        min = new Vector3(0.1f, 0.1f, 0.1f);
-        max = new Vector3(5.0f, 5.0f, 1.0f);
-        deployRange.transform.localScale = min;
+        SizeReset();
         deploy.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        deploy.SetActive(isCheck);
-
-        if (deploy.activeSelf)
-            deployRange.transform.localScale = Vector3.Lerp(min, max, speed * Time.deltaTime);
+        if (isCheck)
+            SizeBigger();
         else
-            deployRange.transform.localScale = min;
+            SizeSmaller();
     }
 
     public void OnDeployButtonDown() => isCheck = deploy.activeSelf == true ? false : true;
@@ -39,4 +36,39 @@ public class Deployment : MonoBehaviour
     public void OnNum2ButtonDown() => Debug.Log("Num2 Button Down");
     public void OnNum3ButtonDown() => Debug.Log("Num3 Button Down");
     public void OnNum4ButtonDown() => Debug.Log("Num4 Button Down");
+
+    public void SizeBigger()
+    {
+        deploy.SetActive(isCheck);
+
+        if (time >= 1f)
+        {
+            time = 1; 
+            return;
+        }
+        
+        deployRange.transform.localScale = Vector3.one * (1 * time);
+        deployRange.transform.Rotate(0, 0, rotate * rotateSpeed * Time.deltaTime);
+        time += Time.deltaTime;
+    }
+
+    public void SizeSmaller()
+    {
+        if (time <= 0f)
+        {
+            time = 0;
+            deploy.SetActive(isCheck);
+            return;
+        }
+        deployRange.transform.localScale = Vector3.one * (1 * time);
+        deployRange.transform.Rotate(0, 0, -rotate * rotateSpeed * Time.deltaTime);
+        time -= Time.deltaTime;
+    }
+
+    public void SizeReset()
+    {
+        time = 0;
+        deployRange.transform.localScale = Vector3.one;
+        deployRange.transform.rotation = Quaternion.identity;
+    }
 }
