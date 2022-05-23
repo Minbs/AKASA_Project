d5300ab03c49b54236f16d00d23e7df94b77bbc2
ApexLegendIsFun <90172140+ApexLegendIsFun@users.noreply.github.com>
2022-05-23 23:47:01 +0900

    데이터1차

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -73,6 +73,7 @@ public class GameManager : Singleton<GameManager>
 
     void Start()
     {
+        Time.timeScale = 2;
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
         foreach (var e in enemies)
             enemiesList.Add(e);
