894963958a90771b35881c3501a7e4de782d2c50
DESKTOP-6OSQBMT\Now <lsd200509@gmail.com>
2022-06-14 00:23:36 +0900

    유닛 업그레이드 버그 고침 게임매니저 들어가서 // <<이거 지우면됨.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -468,7 +468,7 @@ public class GameManager : Singleton<GameManager>
 
         foreach (var m in minionsList)
         {
-           // SynergyManager.Instance.CheckClassSynergy(m);
+            SynergyManager.Instance.CheckClassSynergy(m);
             m.transform.GetChild(0).GetComponent<BoxCollider>().enabled = true;
             m.GetComponent<UnitStateMachine>().agent.enabled = true;
         }
