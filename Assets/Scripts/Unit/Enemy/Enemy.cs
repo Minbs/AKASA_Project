61d9a0bea8e2b9a1e6a7f5fcca78f171cf2c0d63
NOW2-400\NOW2-400 <qudtjr208@gmail.com>
2022-06-13 19:36:29 +0900

    보스 커밋

--- Assets/Scripts/Unit/Enemy/Enemy.cs
+++ Assets/Scripts/Unit/Enemy/Enemy.cs
@@ -8,6 +8,13 @@ using Event = Spine.Event;
 
 public class Enemy : Unit
 {
+    [Header("보스 스킬 변수")]
+    public float skillCoolTime;
+    public float skillRangeWidth;
+    public float skillRangeHeight;
+    public float skillValue;
+
+    public float skillTimer { get; set; }
     // Start is called before the first frame update
 
     protected override void Awake()
@@ -18,6 +25,7 @@ public class Enemy : Unit
     protected override void Start()
     {
         base.Start();
+        skillTimer = 0;
         transform.GetChild(0).GetComponent<SkeletonAnimation>().state.Event += AnimationSatateOnEvent;
         Init();
         UpdateHealthbar();
@@ -40,7 +48,24 @@ public class Enemy : Unit
             return;
         }
 
+        if(GameManager.Instance.state.Equals(State.BATTLE))
+        skillTimer += Time.deltaTime * GameManager.Instance.gameSpeed;
+    }
 
+    public void SkillRangeAttack()
+    {
+        Vector3 box = new Vector3(skillRangeWidth, 1, skillRangeHeight);
+        Vector3 center = transform.position;
+        center.x = transform.position.x + skillRangeWidth / 2;
+        Collider[] targets = Physics.OverlapBox(center, box, Quaternion.identity);
+
+        foreach (var e in targets)
+        {
+            if (!e.transform.parent.tag.Equals("Ally")) continue;
+
+            e.transform.parent.GetComponent<Object>().Deal(currentAtk * skillValue / 100);
+
+        }
     }
 
     void MeleeAttack()
@@ -55,7 +80,6 @@ public class Enemy : Unit
 
     public void MeleeRangeAttack()
     {
-   //     Debug.Log(attackRange2);
         Vector3 box = new Vector3(attackRangeDistance, 1, attackRange2);
         Vector3 center = transform.position;
         center.x = transform.position.x + attackRangeDistance / 2;
@@ -65,7 +89,7 @@ public class Enemy : Unit
         {
             if (!e.transform.parent.tag.Equals("Ally")) continue;
 
-            e.transform.parent.GetComponent<Unit>().Deal(currentAtk);
+            e.transform.parent.GetComponent<Object>().Deal(currentAtk);
 
         }
     }
@@ -91,12 +115,19 @@ public class Enemy : Unit
 
     public void AnimationSatateOnEvent(TrackEntry trackEntry, Event e)
     {
+        if (e.Data.Name == "shoot" && transform.GetChild(0).GetComponent<SkeletonAnimation>().AnimationName == skinName + "/skill")
+        {
+            Debug.Log("스킬");
+            SkillRangeAttack();
+        }
+
+
         if (target == null)
         {
             return;
         }
 
-        if (e.Data.Name == "shoot")
+        if (e.Data.Name == "shoot" && transform.GetChild(0).GetComponent<SkeletonAnimation>().AnimationName == skinName + "/attack")
         {
             if(target != GameManager.Instance.turret)
             target.GetComponent<UnitStateMachine>().SetAttackTargetInRange(gameObject);
